5e53ce4276446fe044ba31a197eef4753ee71ff9
boyninja1555 <flappyfloorg@gmail.com>
2026-08-08 14:32:10 -0500

    Boldified shellyfloor prompt

--- shellyfloor/shellyfloor.c
+++ shellyfloor/shellyfloor.c
@@ -169,7 +169,7 @@ int main()
         getcwd(cwd, sizeof(cwd));
         cwd[sizeof(cwd) - 1] = '\0';
 
-        printf("\x1b[32msf@FloorOS\x1b[0m:\x1b[34m%s\x1b[0m$ ", cwd);
+        printf("\x1b[1;32msf@FloorOS\x1b[0m:\x1b[1;34m%s\x1b[0m$ ", cwd);
         fflush(stdout);
 
         char command[MAX_COMMAND_LENGTH];
